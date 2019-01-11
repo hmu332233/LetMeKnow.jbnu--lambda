ca19e9c97158d4776c2dca5562b745b9cd92c6e6
minung.han <hmu332233@gmail.com>
2019-01-12 04:09:50 +0900

    [Modify] 사용기록 api 호출하도록 수정

--- ncloud/index.js
+++ ncloud/index.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const request = require('request');
 
 const data = [
  {
@@ -9,10 +10,24 @@ const data = [
 ];
 
 function main(params) {
+  const userId = params.userRequest.user.id;
+  const utterance = params.userRequest.utterance;
+  
+  sendMessage({
+    id: userId,
+    content: utterance
+  });
+  
+
   const name = params.action.detailParams.department_name.value;
   const info = _.find(data, { name });
   return {
     "version": "2.0",
     "data": info
   };
+}
+
+const BOT_URL = '';
+function sendMessage({ id, content }) {
+  request.post(BOT_URL, {json:{ id, content }})
 }
