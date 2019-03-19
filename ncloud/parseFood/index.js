71ed8b55103249aab5584349a4d09c664512619d
minung.han <hmu332233@gmail.com>
2019-03-20 00:48:01 +0900

    [Modify] db에 데이터 저장하도록 수정

--- ncloud/parseFood/index.js
+++ ncloud/parseFood/index.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const MongoClient = require('mongodb').MongoClient;
 
 function parseTable($, $table, { place }) {
   const $menus = $table.find('td[bgcolor=#ffffff]');
@@ -9,16 +10,25 @@ function parseTable($, $table, { place }) {
     $element.find('br').replaceWith("\n");
     return $element.text().trim();
   });
-  return [
-    { place, week: '월', time: '중식', category: '백반', menus: menus[0] },
-    { place, week: '화', time: '중식', category: '백반', menus: menus[1] },
-    { place, week: '수', time: '중식', category: '백반', menus: menus[2] },
-    { place, week: '목', time: '중식', category: '백반', menus: menus[3] },
-    { place, week: '금', time: '중식', category: '백반', menus: menus[4] },
-  ]
+  return {
+    lunch: [
+      { place, week: '월', time: '중식', category: '백반', menus: menus[0] },
+      { place, week: '화', time: '중식', category: '백반', menus: menus[1] },
+      { place, week: '수', time: '중식', category: '백반', menus: menus[2] },
+      { place, week: '목', time: '중식', category: '백반', menus: menus[3] },
+      { place, week: '금', time: '중식', category: '백반', menus: menus[4] },
+    ],
+    dinner: [
+      { place, week: '월', time: '석식', category: '백반', menus: menus[6] },
+      { place, week: '화', time: '석식', category: '백반', menus: menus[7] },
+      { place, week: '수', time: '석식', category: '백반', menus: menus[8] },
+      { place, week: '목', time: '석식', category: '백반', menus: menus[9] },
+      { place, week: '금', time: '석식', category: '백반', menus: menus[10] },
+    ],
+  }
 }
 
-function main(params) {
+function getMenus(callback) {
   request('http://sobi.chonbuk.ac.kr/chonbuk/m040101', (err, res, body) => {
     const $ = cheerio.load(body);
 
@@ -29,10 +39,47 @@ function main(params) {
     const $hu = $table.eq(3);
     const $jungdam = $table.eq(4);
     
- 
-    console.log(parseTable($, $jinsu, { place: '진수당' }));
+    const jinsuData = parseTable($, $jinsu, { place: '진수당' });
+    const mediData = parseTable($, $medi, { place: '의대' });
+    const studentHallData = parseTable($, $studentHall, { place: '학생회관' });
+
+    callback({
+      jinsuData,
+      mediData,
+      studentHallData
+    });
   });
 }
 
-main();
+async function insertDocument(db, { collectionName, data }) {
+  const collection = db.collection(collectionName);
+  await collection.insertOne(data);
+  console.log('complete!', collectionName)
+}
 
+function main(params) {
+  getMenus(({ jinsuData, mediData, studentHallData }) => {
+  
+    const url = '';
+    MongoClient.connect(url, { useNewUrlParser: true }, async (err, client) => {
+
+      console.log("Connected successfully to server");
+    
+      const db = client.db('menus');
+
+      await insertDocument(db, { collectionName: 'jinsu', data: jinsuData});
+      await insertDocument(db, { collectionName: 'medi', data: mediData});
+      await insertDocument(db, { collectionName: 'studentHall', data: studentHallData});
+
+      db.on('close', () => {
+        console.log('Disconnected to server');
+      });
+
+      client.close();
+    });
+  });
+
+  
+}
+
+main();
