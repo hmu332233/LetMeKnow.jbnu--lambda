be0970eacbca4bc020e3ed09acade7219e7c5cc2
minung.han <hmu332233@gmail.com>
2019-04-19 01:00:09 +0900

    [Modify] parse logic

--- ncloud/parseFood/index.js
+++ ncloud/parseFood/index.js
@@ -19,11 +19,11 @@ function parseTable($, $table, { place }) {
       { place, week: '금', time: '중식', category: '백반', menus: menus[4] },
     ],
     dinner: [
-      { place, week: '월', time: '석식', category: '백반', menus: menus[6] },
-      { place, week: '화', time: '석식', category: '백반', menus: menus[7] },
-      { place, week: '수', time: '석식', category: '백반', menus: menus[8] },
-      { place, week: '목', time: '석식', category: '백반', menus: menus[9] },
-      { place, week: '금', time: '석식', category: '백반', menus: menus[10] },
+      { place, week: '월', time: '석식', category: '백반', menus: menus[5] },
+      { place, week: '화', time: '석식', category: '백반', menus: menus[6] },
+      { place, week: '수', time: '석식', category: '백반', menus: menus[7] },
+      { place, week: '목', time: '석식', category: '백반', menus: menus[8] },
+      { place, week: '금', time: '석식', category: '백반', menus: menus[9] },
     ],
   }
 }
