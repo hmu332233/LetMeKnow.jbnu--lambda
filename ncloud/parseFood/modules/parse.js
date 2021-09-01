df1d47984a09c515dc466d59e98a827910c44c60
minungHan <hmu332233@gmail.com>
2021-09-02 00:49:32 +0900

    feat: 파싱 로직 수정

--- ncloud/parseFood/modules/parse.js
+++ ncloud/parseFood/modules/parse.js
@@ -1,37 +1,48 @@
 const cheerio = require('cheerio');
 
 function parseTable($, $table) {
-  const $menus = $table.find('td[bgcolor=#ffffff]');
+  const $trList = $table.find('tbody > tr');
+  console.log($trList.length, '--')
 
-  const menus = $menus.map((index, element) => {
-    const $element = $(element);
-    $element.find('br').replaceWith("\n");
-    return $element.text().trim();
+  const menus = [];
+  $trList.map((index, e1) => {
+    const $tdList = $(e1).find('td');
+    // console.log($tdList.length)
+    $tdList.map((index, e2) => {
+
+      const $liList = $(e2).find('li');
+
+      if ($liList.length === 0) {
+        menus.push($(e2).text());
+      } else {
+        const menusOfDay =  $liList.map((index, e) => $(e).text()).toArray();
+        menus.push(menusOfDay.join('\n'))
+      }
+
+    });
   });
 
+  console.log(menus)
   return menus;
 }
 
 function parseMenus(html) {
   const $ = cheerio.load(html);
 
-  const $table = $('#sub_right table');
+  const $table = $('.menu_scrollArea table');
   const $jinsu = $table.eq(0);
   const $medi = $table.eq(1);
-  const $studentHall = $table.eq(2);
-  const $hu = $table.eq(3);
-  const $jungdam = $table.eq(4);
+  const $hu = $table.eq(2);
+  const $jungdam = $table.eq(3);
   
   const jinsuMenus = parseTable($, $jinsu);
   const mediMenus = parseTable($, $medi);
-  const studentHallMenus = parseTable($, $studentHall);
   const huMenus = parseTable($, $hu);
   const jungdamMenus = parseTable($, $jungdam);
 
   return {
     jinsuMenus,
     mediMenus,
-    studentHallMenus,
     huMenus,
     jungdamMenus
   };
