87fc46e77d1bbe53814b2bae30f3b4f6ed19df3e
DESKTOP-S90ELTE\user <jinseok1006@naver.com>
2024-01-03 22:03:07 +0900

    fix: 생협 주소 변경에 따른 파서 내용 수정

--- ncloud/parseFood/modules/parse.js
+++ ncloud/parseFood/modules/parse.js
@@ -2,28 +2,21 @@ const cheerio = require('cheerio');
 
 function parseTable($, $table) {
   const $trList = $table.find('tbody > tr');
-  console.log($trList.length, '--')
 
+  const getLine = (e) => $(e).html().split('<br>').join('\n');
   const menus = [];
-  $trList.map((index, e1) => {
+
+  $trList.each((_, e1) => {
     const $tdList = $(e1).find('td');
-    // console.log($tdList.length)
-    $tdList.map((index, e2) => {
 
-      const $liList = $(e2).find('li');
-
-      if ($liList.length === 0) {
-        menus.push($(e2).html().split('<br>').join('\n'));
-      } else {
-        const menusOfDay =  $liList.map((index, e) => $(e).text()).toArray();
-        menus.push(menusOfDay.join('\n'))
-      }
-
+    $tdList.each((_, e2) => {
+      if ($(e2).find('span').length > 0)
+        menus.push(getLine($(e2).find('span')));
+      else menus.push(getLine(e2));
     });
   });
 
-  console.log(menus)
-  return menus.map(v => v.trim());
+  return menus.map((v) => v.trim());
 }
 
 function parseMenus(html) {
@@ -33,22 +26,19 @@ function parseMenus(html) {
   const $jinsu = $table.eq(0);
   const $medi = $table.eq(1);
   const $hu = $table.eq(2);
-  const $jungdam = $table.eq(3);
-  
+
   const jinsuMenus = parseTable($, $jinsu);
   const mediMenus = parseTable($, $medi);
   const huMenus = parseTable($, $hu);
-  const jungdamMenus = parseTable($, $jungdam);
 
   return {
     jinsuMenus,
     mediMenus,
     huMenus,
-    jungdamMenus
   };
 }
 
 module.exports = {
   parseMenus,
-  parseTable
-}
+  parseTable,
+};
