5187e3a867f181b153592936eafff1332edd721e
DESKTOP-S90ELTE\user <jinseok1006@naver.com>
2024-01-05 03:20:49 +0900

    fix: normalize 모듈 후생관 메뉴 개수 조정

--- ncloud/parseFood/modules/normalize.js
+++ ncloud/parseFood/modules/normalize.js
@@ -30,58 +30,43 @@ function normalizeHu(place = '후생관', menus) {
       { place, week: '월', time: '중식', category: '찌개', menus: menus[5] },
       { place, week: '월', time: '중식', category: '돌솥', menus: menus[10] },
       { place, week: '월', time: '중식', category: '특식', menus: menus[15] },
-      // { place, week: '월', time: '중식', category: '도시락', menus: menus[20] },
-      // { place, week: '월', time: '중식', category: '덮밥 비빔밥', menus: menus[25].split('/').join('\n') },
       { place, week: '월', time: '중식', category: '샐러드', menus: menus[30] },
-      // { place, week: '월', time: '중식', category: '돈까스', menus: menus[35] },
-      { place, week: '월', time: '중식', category: '오므라이스', menus: menus[36]+'\n'+menus[37] },
+      // { place, week: '월', time: '중식', category: '오므라이스', menus: menus[37] },
 
       // 화
       { place, week: '화', time: '중식', category: '찌개', menus: menus[6] },
       { place, week: '화', time: '중식', category: '돌솥', menus: menus[11] },
       { place, week: '화', time: '중식', category: '특식', menus: menus[16] },
-      // { place, week: '화', time: '중식', category: '도시락', menus: menus[21] },
-      // { place, week: '화', time: '중식', category: '덮밥 비빔밥', menus: menus[26].split('/').join('\n') },
       { place, week: '화', time: '중식', category: '샐러드', menus: menus[31] },
-      // { place, week: '월', time: '중식', category: '돈까스', menus: menus[35] },
-      { place, week: '화', time: '중식', category: '오므라이스', menus: menus[36]+'\n'+menus[38] },
+      // { place, week: '화', time: '중식', category: '오므라이스', menus: menus[38] },
 
       // 수
       { place, week: '수', time: '중식', category: '찌개', menus: menus[7] },
       { place, week: '수', time: '중식', category: '돌솥', menus: menus[12] },
       { place, week: '수', time: '중식', category: '특식', menus: menus[17] },
-      // { place, week: '수', time: '중식', category: '도시락', menus: menus[22] },
-      // { place, week: '수', time: '중식', category: '덮밥 비빔밥', menus: menus[27].split('/').join('\n') },
       { place, week: '수', time: '중식', category: '샐러드', menus: menus[32] },
-      // { place, week: '월', time: '중식', category: '돈까스', menus: menus[35] },
-      { place, week: '수', time: '중식', category: '오므라이스', menus: menus[36]+'\n'+menus[39] },
+      // { place, week: '수', time: '중식', category: '오므라이스', menus: menus[39] },
 
       // 목 
       { place, week: '목', time: '중식', category: '찌개', menus: menus[8] },
       { place, week: '목', time: '중식', category: '돌솥', menus: menus[13] },
       { place, week: '목', time: '중식', category: '특식', menus: menus[18] },
-      // { place, week: '목', time: '중식', category: '도시락', menus: menus[23] },
-      // { place, week: '목', time: '중식', category: '덮밥 비빔밥', menus: menus[28].split('/').join('\n') },
       { place, week: '목', time: '중식', category: '샐러드', menus: menus[33] },
-      // { place, week: '월', time: '중식', category: '돈까스', menus: menus[35] },
-      { place, week: '금', time: '중식', category: '오므라이스', menus: menus[36]+'\n'+menus[40] },
+      // { place, week: '목', time: '중식', category: '오므라이스', menus: menus[40] },
 
       // 금
       { place, week: '금', time: '중식', category: '찌개', menus: menus[9] },
       { place, week: '금', time: '중식', category: '돌솥', menus: menus[14] },
       { place, week: '금', time: '중식', category: '특식', menus: menus[19] },
-      // { place, week: '금', time: '중식', category: '도시락', menus: menus[24] },
-      // { place, week: '금', time: '중식', category: '덮밥 비빔밥', menus: menus[29].split('/').join('\n') },
       { place, week: '금', time: '중식', category: '샐러드', menus: menus[34] },
-      // { place, week: '월', time: '중식', category: '돈까스', menus: menus[35] },
-      { place, week: '금', time: '중식', category: '오므라이스', menus: menus[36]+'\n'+menus[41] },
+      // { place, week: '금', time: '중식', category: '오므라이스', menus: menus[41] },
     ],
     dinner: [
-      { place, week: '월', time: '석식', category: '백반', menus: menus[20] },
-      { place, week: '화', time: '석식', category: '백반', menus: menus[21] },
-      { place, week: '수', time: '석식', category: '백반', menus: menus[22] },
-      { place, week: '목', time: '석식', category: '백반', menus: menus[23] },
-      { place, week: '금', time: '석식', category: '백반', menus: menus[24] },
+      { place, week: '월', time: '석식', category: '백반', menus: '운영 없음' },
+      { place, week: '화', time: '석식', category: '백반', menus: '운영 없음' },
+      { place, week: '수', time: '석식', category: '백반', menus: '운영 없음' },
+      { place, week: '목', time: '석식', category: '백반', menus: '운영 없음' },
+      { place, week: '금', time: '석식', category: '백반', menus: '운영 없음' },
     ]
   }
 }
